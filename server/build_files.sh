512700a1fb30a59a0e9458c20e0fcb09c62c8139
Pradeep Pawar <72540482+pradeep8577@users.noreply.github.com>
2023-06-27 22:27:07 +0530

    Update build_files.sh

--- server/build_files.sh
+++ server/build_files.sh
@@ -4,9 +4,3 @@ python3.9 manage.py collectstatic --noinput --clear
 echo " BUILD END" 
 
 
-app = application
-
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
-
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
